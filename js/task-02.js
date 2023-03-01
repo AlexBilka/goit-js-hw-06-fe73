15e69106eee458566ba1a6ee3899e84332806c95
AlexBilka <aleksandrbelka@gmail.com>
2023-03-02 00:05:40 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.getElementById("ingredients");
+
+const liElements = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+
+  li.textContent = ingredient;
+
+  li.classList.add("item");
+
+  return li;
+});
+
+ul.append(...liElements);
