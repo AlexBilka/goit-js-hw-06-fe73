7c5761355b31c3face44ed6f7ac3dcb96d9f25d7
AlexBilka <aleksandrbelka@gmail.com>
2023-03-02 03:35:49 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,11 @@
 const categoriesList = document.querySelector("#categories");
 const categoriesItems = categoriesList.querySelectorAll(".item");
 
-console.log(`Кількість категорій: ${categoriesItems.length}`);
+console.log(`Number of categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
   const categoryItemCount = category.querySelectorAll("li").length;
 
-  console.log(
-    `Категорія: ${categoryName}; кількість елементів: ${categoryItemCount}`
-  );
+  console.log(`Category: ${categoryName} \nElements: ${categoryItemCount}`);
 });
